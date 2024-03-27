ec56dfa4087a90e074d1295f6b8d66d83b88806a
Jack Sage-Phoenix <jack@jackmeyers.co>
2024-03-27 13:09:17 -0500

    Scheduling tasks

---
+++ scheduling-tasks/src/main/java/com/p3soft/schedulingtasks/ScheduledTasks.java
@@ -0,0 +1,21 @@
+package com.p3soft;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    private static final SimpleDateFormat dForm = new SimpleDateFormat("HHmm.ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        logger.info("The time is now {}", dForm.format(new Date()));
+    }
+    
+}
---
+++ scheduling-tasks/src/main/java/com/p3soft/schedulingtasks/SchedulingTasksApplication.java
@@ -0,0 +1,15 @@
+package com.p3soft.schedulingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+d
+
+@SpringBootApplication
+public class SchedulingTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTasksApplication.class, args);
+	}
+
+}
